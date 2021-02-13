e3bf88a120bf19e1c2da9eea9aa895ce0f961f49
Chen <martinezchen01@icloud.com>
2021-02-13 11:47:26 +0800

    SpringBoot 23
    boot-05-web init

--- boot-01-helloworld/src/main/java/com/martin/boot/bean/Car.java
+++ boot-01-helloworld/src/main/java/com/martin/boot/bean/Car.java
@@ -1,6 +1,8 @@
 package com.martin.boot.bean;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +12,8 @@ import org.springframework.stereotype.Component;
 @Data
 @Component
 @ConfigurationProperties(prefix = "mycar")
+@AllArgsConstructor  //有參數建構子
+@NoArgsConstructor //無參數建構子
 public class Car {
     private String brand;
     private Integer price;
